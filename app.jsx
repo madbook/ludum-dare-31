4bb08e78d70f79dbac620a5d473f85cfb06d70f2
Matt Lee <matt@reddit.com>
2014-12-06 15:48:57 -0800

    Render an empty board.

--- app.jsx
+++ app.jsx
@@ -3,14 +3,46 @@ React.initializeTouchEvents(true);
 $(function() {
   var mountNode = document.getElementById('app');
 
+  var activeBoard = [];
+  var i = 16;
+  while (i--) {
+    activeBoard.push(1);
+  }
+
   var App = React.createClass({
+    getInitialState: function() {
+      return {
+        activeBoard: activeBoard,
+      };
+    },
+    
     render: function() {
-      return null;
+      return <Board cells={this.state.activeBoard} />;
+    },
+  });
+
+  var Cell = React.createClass({
+    render: function() {
+      return <div className="cell">
+        <canvas className="cell-contents" width="100" height="100" />
+      </div>;
     },
   });
   
+  var Board = React.createClass({
+    render: function() {
+      return <div className="board">
+        {this.props.cells.map(this.renderCell)}
+      </div>;
+    },
+
+    renderCell: function(cell) {
+      return <Cell {...cell} />;
+    },
+  });
+
   React.render(
-    <App query={query} />,
+    <App />,
     mountNode
   );
 });
