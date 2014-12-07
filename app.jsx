b076a8c435d7dd95d25f1d41ae272856a73a1f7f
Matt Lee <matt@reddit.com>
2014-12-06 16:40:03 -0800

    Add level rendering with directory navigation.

--- app.jsx
+++ app.jsx
@@ -3,29 +3,83 @@ React.initializeTouchEvents(true);
 $(function() {
   var mountNode = document.getElementById('app');
 
-  var activeBoard = [];
+  var blankBoard = [];
+  
   var i = 16;
   while (i--) {
-    activeBoard.push(1);
+    blankBoard.push(1);
+  }
+
+  var Shell = {
+    directory: blankBoard.slice(),
+
+    clearDirectory: function() {
+      var i = 16;
+      while (i--) {
+        Shell.directory[i] = 1;
+      }
+    },
+
+    changeDirectory: function(directory) {
+      var l = directory.data.length;
+      var offset = directory.offset;
+      var i;
+      
+      Shell.clearDirectory();
+
+      while (l--) {
+        i = (l + offset) % 16;
+        Shell.directory[i] = directory.data[l];
+      }
+
+      Shell.fireCallbacks(Shell.onChangeDirectoryCallbacks);
+    },
+
+    onChangeDirectoryCallbacks: [],
+
+    fireCallbacks: function(arr) {
+      arr.forEach(function(cb) { cb(); });
+    },
   }
 
   var App = React.createClass({
     getInitialState: function() {
       return {
-        activeBoard: activeBoard,
+        activeBoard: blankBoard.slice(),
+        activeDirectory: this.props.levelData,
       };
     },
     
+    componentWillMount: function() {
+      Shell.onChangeDirectoryCallbacks.push(this.onShellDirectoryChange);
+      Shell.changeDirectory(this.state.activeDirectory);
+    },
+
     render: function() {
       return <Board cells={this.state.activeBoard} />;
+    },
+
+    onShellDirectoryChange: function() {
+      this.setState({
+        activeBoard: Shell.directory,
+      });
     },
   });
 
   var Cell = React.createClass({
     render: function() {
-      return <div className="cell">
+      return <a className={'cell ' + this.props.type}
+                onClick={this.handleClick}>
         <canvas className="cell-contents" width="100" height="100" />
-      </div>;
+      </a>;
+    },
+
+    handleClick: function() {
+      if (this.props.type === 'directory') {
+        Shell.changeDirectory(this.props);
+      } else if (this.props.type === 'linked_directory') {
+        Shell.changeDirectory(this.props.link);
+      }
     },
   });
   
@@ -41,8 +95,35 @@ $(function() {
     },
   });
 
-  React.render(
-    <App />,
-    mountNode
-  );
+  function decorateLevelData(data, parent) {
+    if (data.type === 'directory' && parent) {
+      // set offset for child directories
+      data.offset = parent.offset + parent.data.indexOf(data);
+      
+      // add ../ directory
+      data.data.unshift({
+        name: '../',
+        type: 'linked_directory',
+        link: parent,
+      });
+    }
+
+    if (data.type === 'directory' || data.type === 'root_directory') {
+      // call recursively
+      data.data.forEach(function(child) {
+        decorateLevelData(child, data);
+      });
+    }
+  }
+
+  $.getJSON('level1.json').then(function(data) {
+    decorateLevelData(data, null);
+    React.render(
+      <App levelData={data} />,
+      mountNode
+    );
+  }).fail(function(err) {
+    debugger;
+  });
+
 });
