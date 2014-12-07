f93ee8b96615e9a2fa6205793d3962babf42cb5a
Matt Lee <matt@reddit.com>
2014-12-06 17:44:41 -0800

    Add move and decrypt scripts.

--- app.jsx
+++ app.jsx
@@ -10,13 +10,69 @@ $(function() {
     blankBoard.push(1);
   }
 
+  var Bin = {
+    'change_directory': {
+      name: 'change_directory',
+      arguments: [
+        ['directory', 'linked_directory']
+      ],
+      source: function(target) {
+        if (target.type === 'linked_directory') {
+          target = target.link;
+        }
+
+        this.changeDirectory(target);
+      }
+    },
+
+    'move': {
+      name: 'move',
+      arguments: [
+        ['file', 'key_file'],
+        ['directory', 'linked_directory']
+      ],
+      source: function(file, target) {
+        if (target.type === 'linked_directory') {
+          target = target.link;
+        }
+        
+        var i = file.parent.listing.indexOf(file.name);
+        file.parent.data.splice(i, 1);
+        file.parent.listing.splice(i, 1);
+        file.parent = target;
+        target.data.push(file);
+        target.listing.push(file.name);
+        this.refresh();
+      }
+    },
+
+    'decrypt': {
+      name: 'decrypt',
+      arguments: [
+        ['key_file'],
+        ['encrypted_file']
+      ],
+      source: function(key, file) {
+        var i = file.parent.listing.indexOf(file.name);
+        file.parent.data[i].type = 'file';
+        this.refresh();
+      }
+    }
+  };
+
   var Shell = {
-    directory: blankBoard.slice(),
+    directory: null,
+
+    listing: blankBoard.slice(),
+
+    script: null,
+
+    scriptParams: [],
 
     clearDirectory: function() {
       var i = 16;
       while (i--) {
-        Shell.directory[i] = 1;
+        Shell.listing[i] = 1;
       }
     },
 
@@ -29,17 +85,61 @@ $(function() {
 
       while (l--) {
         i = (l + offset) % 16;
-        Shell.directory[i] = directory.data[l];
+        Shell.listing[i] = directory.data[l];
       }
 
+      Shell.directory = directory;
       Shell.fireCallbacks(Shell.onChangeDirectoryCallbacks);
     },
 
     onChangeDirectoryCallbacks: [],
 
+    refresh: function() {
+      Shell.changeDirectory(Shell.directory);
+    },
+
     fireCallbacks: function(arr) {
       arr.forEach(function(cb) { cb(); });
     },
+
+    interact: function(obj) {
+      console.log('interacting with ' + obj.type);
+      
+      if (!Shell.script) {
+        if (obj.type === 'script' && Bin[obj.name]) {
+          console.log('setting script to ' + obj.name);
+          Shell.script = Bin[obj.name];
+          Shell.scriptParams.length = 0;
+          console.log('');
+          return;
+        } else {
+          console.log('setting default script to change_directory');
+          Shell.script = Bin.change_directory;
+          Shell.scriptParams.length = 0;
+        }
+      }
+
+      var argNum = Shell.scriptParams.length;
+      console.log('checking argument number ' + argNum)
+
+      if (Shell.script.arguments[argNum] && Shell.script.arguments[argNum].indexOf(obj.type) >= 0) {
+        Shell.scriptParams.push(obj);
+        console.log('argument valid, adding to param list');
+
+        if (Shell.scriptParams.length === Shell.script.arguments.length) {
+          console.log('param list complete, calling method ' + Shell.script.name);
+          Shell.script.source.apply(Shell, Shell.scriptParams);
+          Shell.script = null;
+          Shell.scriptParams.length = 0;
+        }
+      } else {
+        console.log('argument not valid, aborting');
+        Shell.script = null;
+        Shell.scriptParams.length = 0;
+      }
+
+      console.log('');
+    }
   }
 
   var App = React.createClass({
@@ -61,7 +161,7 @@ $(function() {
 
     onShellDirectoryChange: function() {
       this.setState({
-        activeBoard: Shell.directory,
+        activeBoard: Shell.listing,
       });
     },
   });
@@ -75,11 +175,7 @@ $(function() {
     },
 
     handleClick: function() {
-      if (this.props.type === 'directory') {
-        Shell.changeDirectory(this.props);
-      } else if (this.props.type === 'linked_directory') {
-        Shell.changeDirectory(this.props.link);
-      }
+      Shell.interact(this.props);
     },
   });
   
@@ -96,6 +192,10 @@ $(function() {
   });
 
   function decorateLevelData(data, parent) {
+    if (parent && !data.parent) {
+      data.parent = parent;
+    }
+
     if (data.type === 'directory' && parent) {
       // set offset for child directories
       data.offset = parent.offset + parent.data.indexOf(data);
@@ -109,6 +209,10 @@ $(function() {
     }
 
     if (data.type === 'directory' || data.type === 'root_directory') {
+      data.listing = data.data.map(function(child) {
+        return child.name;
+      });
+
       // call recursively
       data.data.forEach(function(child) {
         decorateLevelData(child, data);
